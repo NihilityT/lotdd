673198d2c186e3a305eca1fedcd2bfbdd072a0df
Nihility <Nihility.t@gmail.com>
2022-04-15 00:34:30 +0800

    c2/28

--- c2/SoundexTest.cpp
+++ c2/SoundexTest.cpp
@@ -33,3 +33,6 @@ TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
    ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u)); 
 }
 
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
+   ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+}
