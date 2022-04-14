555a78653bf0350c4f3f8caba7e59083b56984db
Nihility <Nihility.t@gmail.com>
2022-04-15 00:34:37 +0800

    c3/5

--- c3/RetweetCollectionTest.cpp
+++ c3/RetweetCollectionTest.cpp
@@ -13,17 +13,17 @@ TEST_F(ARetweetCollection, IsEmptyWhenCreated) {
    ASSERT_TRUE(collection.isEmpty());
 }
 
-TEST_F(ARetweetCollection, HasSizeZeroWhenCreated) {
-   ASSERT_THAT(collection.size(), Eq(0u));
-}
-
 TEST_F(ARetweetCollection, IsNoLongerEmptyAfterTweetAdded) {
    collection.add(Tweet());
    
    ASSERT_FALSE(collection.isEmpty());
 }
 
-TEST_F(ARetweetCollection, HasSizeOfOneAfterTweetAdded) {
+TEST_F(ARetweetCollection, HasSizeZeroWhenCreated) {
+   ASSERT_THAT(collection.size(), Eq(0u));
+}
+
+TEST_F(ARetweetCollection, DISABLED_HasSizeOfOneAfterTweetAdded) {
    collection.add(Tweet());
 
    ASSERT_THAT(collection.size(), Eq(1u));
