d6e5b0da2afc82842f073e1e7640373362502472
Nihility <Nihility.t@gmail.com>
2022-04-15 00:34:25 +0800

    c2/8

--- c2/SoundexTest.cpp
+++ c2/SoundexTest.cpp
@@ -4,16 +4,19 @@ class Soundex
 {
 public:
    std::string encode(const std::string& word) const {
-      return "A";
+      return word;
    }
 };
 
-#include "gmock/gmock.h"
+#include "gmock/gmock.h"    
+
 using ::testing::Eq;
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
    Soundex soundex;
+   
    auto encoded = soundex.encode("A");
+
    ASSERT_THAT(encoded, Eq("A")); 
 }
 
