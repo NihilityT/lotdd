372d0f39fbc1fbe113e61d37c20bb09eaa8e77ec
Nihility <Nihility.t@gmail.com>
2022-04-15 00:34:29 +0800

    c2/21

--- c2/SoundexTest.cpp
+++ c2/SoundexTest.cpp
@@ -21,3 +21,6 @@ TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
    ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
 }
 
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+   ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
