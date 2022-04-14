4eed3ad2b6a6ed7d956ed2c89951fec580c32c2a
Nihility <Nihility.t@gmail.com>
2022-04-15 00:35:18 +0800

    tpp/21

--- tpp/SoundexTest.cpp
+++ tpp/SoundexTest.cpp
@@ -2,33 +2,33 @@
 
 #include "Soundex.h"
 
-TEST_GROUP(SoundexEncoding) {
+TEST_GROUP(ASoundexEncoding) {
    Soundex soundex;
 };
 
-TEST(SoundexEncoding, AppendsZerosToWordForOneLetterWord) {
+TEST(ASoundexEncoding, AppendsZerosToWordForOneLetterWord) {
    CHECK_EQUAL("A000", soundex.encode("A"));
    CHECK_EQUAL("B000", soundex.encode("B"));
 }
 
-TEST(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+TEST(ASoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
    CHECK_EQUAL("A100", soundex.encode("Ab"));
    CHECK_EQUAL("A200", soundex.encode("Ac"));
 }
 
-TEST(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
+TEST(ASoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
    CHECK_EQUAL("A234", soundex.encode("Acdl"));
 }
 
-TEST(SoundexEncoding, IgnoresVowelLikeLetters) {
+TEST(ASoundexEncoding, IgnoresVowelLikeLetters) {
    CHECK_EQUAL("B234", soundex.encode("BAaEeIiOoUuHhYycdl"));
 }
 
-TEST(SoundexEncoding, IgnoresNonAlphabetics) {
+TEST(ASoundexEncoding, IgnoresNonAlphabetics) {
    CHECK_EQUAL("F234", soundex.encode("F987654321%#.=+cdl"));
 }
 
-TEST(SoundexEncoding, CombinesDuplicateEncodings) {
+TEST(ASoundexEncoding, CombinesDuplicateEncodings) {
    CHECK_EQUAL(soundex.codeFor('f'), soundex.codeFor('b'));
    CHECK_EQUAL(soundex.codeFor('g'), soundex.codeFor('c'));
    CHECK_EQUAL(soundex.codeFor('t'), soundex.codeFor('d'));
@@ -36,27 +36,31 @@ TEST(SoundexEncoding, CombinesDuplicateEncodings) {
    CHECK_EQUAL("A123", soundex.encode("Abfcgdt"));
 }
 
-TEST(SoundexEncoding, CombinesMultipleDuplicateEncodings) {
+TEST(ASoundexEncoding, CombinesMultipleDuplicateEncodings) {
    CHECK_EQUAL("J100", soundex.encode("Jbbb"));
 }
 
-TEST(SoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicates1st) {
+TEST(ASoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicates1st) {
    CHECK_EQUAL("B230", soundex.encode("bbcd"));
 }
 
-TEST(SoundexEncoding, UppercasesFirstLetter) {
+TEST(ASoundexEncoding, UppercasesFirstLetter) {
    CHECK_EQUAL("A", soundex.encode("abcd").substr(0, 1));
 }
 
-TEST(SoundexEncoding, IgnoresCaseWhenEncodingConsonants) {
+TEST(ASoundexEncoding, IgnoresCaseWhenEncodingConsonants) {
    CHECK_EQUAL(soundex.encode("BCDL"), soundex.encode("bcdl"));
 }
 
-TEST(SoundexEncoding, DoesNotCombineDuplicateEncodingsSeparatedByVowels) {
+TEST(ASoundexEncoding, DoesNotCombineDuplicateEncodingsSeparatedByVowels) {
    CHECK_EQUAL("J110", soundex.encode("Jbob")); 
 }
 
-TEST(SoundexEncoding, LimitsLengthToFourCharacters) {
+TEST(ASoundexEncoding, LimitsLengthToFourCharacters) {
    CHECK_EQUAL(4u, soundex.encode("Dcdlb").length());
+}
+
+TEST(ASoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+   CHECK_EQUAL("I000", soundex.encode("I"));
 }
 
