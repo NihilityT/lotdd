617599d234ff01ba1669c0e9ae644c1b2e856be9
Nihility <Nihility.t@gmail.com>
2022-04-15 00:35:22 +0800

    wav/22

--- wav/Snippet.h
+++ wav/Snippet.h
@@ -9,13 +9,22 @@
 #ifndef Snippet_h
 #define Snippet_h
 
-using namespace std;
+#include "FileUtil.h"
+#include "WavDescriptor.h"
+#include "WavStructs.h"
+#include "rlog/RLogChannel.h"
+#undef RLOG_SECTION
+#define RLOG_SECTION
+// see https://code.google.com/p/rlog/issues/detail?id=13
+
+#include <string>
+#include <memory>
+#include <algorithm>
 
 class Snippet {
 public:
-   // ... 
-   Snippet(shared_ptr<FileUtil> fileUtil,
-      shared_ptr<WavDescriptor> descriptor,
+   Snippet(std::shared_ptr<FileUtil> fileUtil,
+      std::shared_ptr<WavDescriptor> descriptor,
       const std::string& dest,
       rlog::RLogChannel* channel) 
       : fileUtil_(fileUtil)
@@ -23,8 +32,8 @@ public:
       , dest_(dest) 
       , channel_(channel) { }
 
-   void writeSnippet(
-         const string& name, istream& file, ostream& out,
+   void write(
+         const std::string& name, std::istream& file, std::ostream& out,
          FormatSubchunk& formatSubchunk,
          DataChunk& dataChunk,
          char* data
@@ -36,11 +45,11 @@ public:
       uint32_t samplesToWrite{secondsDesired * formatSubchunk.samplesPerSecond};
       uint32_t totalSamples{dataChunk.length / bytesPerSample};
 
-      samplesToWrite = min(samplesToWrite, totalSamples);
+      samplesToWrite = std::min(samplesToWrite, totalSamples);
 
       uint32_t totalSeconds { totalSamples / formatSubchunk.samplesPerSecond };
 
-      rLog(channel_, "total seconds %i ", totalSeconds);
+      rLog(channel_, "total seconds %u ", totalSeconds);
 
       dataChunk.length = dataLength(
             samplesToWrite, 
@@ -60,8 +69,6 @@ public:
       descriptor_->add(dest_, name, 
             totalSeconds, formatSubchunk.samplesPerSecond, formatSubchunk.channels,
             fileSize);
-      
-      //out.close(); // ostreams are RAII
    }
 
    uint32_t dataLength(
@@ -72,13 +79,12 @@ public:
       return samples * bytesPerSample * channels;
    }
 
-   void writeSamples(ostream* out, char* data, 
+   void writeSamples(std::ostream* out, char* data, 
          uint32_t startingSample, 
          uint32_t samplesToWrite, 
          uint32_t bytesPerSample,
          uint32_t channels=1) {
-      rLog(channel_, "writing %i samples", samplesToWrite);
-      // ...
+      rLog(channel_, "writing %u samples", samplesToWrite);
 
       for (auto sample = startingSample; 
            sample < startingSample + samplesToWrite; 
@@ -94,10 +100,11 @@ public:
    }
 
 private:
-   shared_ptr<FileUtil> fileUtil_;
-   shared_ptr<WavDescriptor> descriptor_;
-   const string dest_;
+   std::shared_ptr<FileUtil> fileUtil_;
+   std::shared_ptr<WavDescriptor> descriptor_;
+   const std::string dest_;
    rlog::RLogChannel* channel_;
 };
 
 #endif
+
