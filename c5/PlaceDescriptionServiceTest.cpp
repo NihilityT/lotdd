edf2477cc647cee247884c5e55d3258b3eaac89c
Nihility <Nihility.t@gmail.com>
2022-04-15 00:34:42 +0800

    c5/5

--- c5/PlaceDescriptionServiceTest.cpp
+++ c5/PlaceDescriptionServiceTest.cpp
@@ -16,17 +16,16 @@ const string APlaceDescriptionService::ValidLatitude("38.005");
 const string APlaceDescriptionService::ValidLongitude("-104.44");
 
 class HttpStub: public Http {
+public:
+   string returnResponse;
    void initialize() override {}
    std::string get(const std::string& url) const override {
       verify(url);
-      return R"({ "address": {
-         "road":"Drury Ln",
-         "city":"Fountain",
-         "state":"CO",
-         "country":"US" }})";
+      return returnResponse;
    }
 
    void verify(const string& url) const {
+      // ...
       string urlStart(
          "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&");
       string expected(urlStart + 
@@ -38,10 +37,13 @@ class HttpStub: public Http {
 
 TEST_F(APlaceDescriptionService, ReturnsDescriptionForValidLocation) {
    HttpStub httpStub;
+   httpStub.returnResponse = R"({"address": {
+                                    "road":"Drury Ln",
+                                    "city":"Fountain",
+                                    "state":"CO",
+                                    "country":"US" }})";
    PlaceDescriptionService service{&httpStub};
-
    auto description = service.summaryDescription(ValidLatitude, ValidLongitude);
-
    ASSERT_THAT(description, Eq("Drury Ln, Fountain, CO, US"));
 }
 
