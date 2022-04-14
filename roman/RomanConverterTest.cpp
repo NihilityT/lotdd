505e0a649ad3cc259d40856ee26975d88d6227dc
Nihility <Nihility.t@gmail.com>
2022-04-15 00:35:10 +0800

    roman/2

--- roman/RomanConverterTest.cpp
+++ roman/RomanConverterTest.cpp
@@ -3,6 +3,11 @@
 using namespace ::testing;
 using namespace std;
 
+string convert(unsigned int arabic)
+{
+   return "I";
+}
+
 TEST(RomanConverter, CanConvertPositiveDigits) {
    EXPECT_THAT(convert(1), Eq("I"));
 }
