86b0ea98ab6992530a60d5863d651d2fc5453686
Nihility <Nihility.t@gmail.com>
2022-04-15 00:34:24 +0800

    c2/7

--- c2/SoundexTest.cpp
+++ c2/SoundexTest.cpp
@@ -8,14 +8,12 @@ public:
    }
 };
 
-
 #include "gmock/gmock.h"
+using ::testing::Eq;
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
    Soundex soundex;
-   
    auto encoded = soundex.encode("A");
-
-   ASSERT_THAT(encoded, testing::Eq("A")); 
+   ASSERT_THAT(encoded, Eq("A")); 
 }
 
