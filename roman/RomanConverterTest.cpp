51abeebfa863f2fad6cb3e71390dc0d249894399
Nihility <Nihility.t@gmail.com>
2022-04-15 00:35:11 +0800

    roman/5

--- roman/RomanConverterTest.cpp
+++ roman/RomanConverterTest.cpp
@@ -6,6 +6,8 @@ using namespace std;
 string convert(unsigned int arabic) 
 {
    string roman{""};
+   if (arabic == 10)
+      return "X";
    while (arabic-- > 0)
       roman += "I";
    return roman;
@@ -15,4 +17,5 @@ TEST(RomanConverter, CanConvertPositiveDigits) {
    EXPECT_THAT(convert(1), Eq("I"));
    EXPECT_THAT(convert(2), Eq("II"));
    EXPECT_THAT(convert(3), Eq("III"));
+   EXPECT_THAT(convert(10), Eq("X"));
 }
