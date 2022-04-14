35dc894274a756480a788073313fc5d529cf28c7
Nihility <Nihility.t@gmail.com>
2022-04-15 00:35:19 +0800

    wav/11

--- wav/WavReaderTest.cpp
+++ wav/WavReaderTest.cpp
@@ -1,4 +1,5 @@
 #include "CppUTest/TestHarness.h"
+
 #include "WavReader.h"
 #include <iostream>
 #include <string>
@@ -28,9 +29,9 @@ TEST(WavReader_WriteSamples, WritesSingleSample) {
    uint32_t bytesPerSample { 1 };
    uint32_t startingSample { 0 };
    uint32_t samplesToWrite { 1 };
-   
+
    reader.writeSamples(&out, data, startingSample, samplesToWrite, bytesPerSample);
-   
+
    CHECK_EQUAL("a", out.str());
 }
 
@@ -51,8 +52,23 @@ TEST(WavReader_WriteSamples, IncorporatesChannelCount) {
    uint32_t startingSample { 0 };
    uint32_t samplesToWrite { 2 };
    uint32_t channels { 2 };
+
    reader.writeSamples(
          &out, data, startingSample, samplesToWrite, bytesPerSample, channels);
+
    CHECK_EQUAL("01234567", out.str());
 }
 
+TEST_GROUP(WavReader_DataLength) {
+   WavReader reader{"",""};
+};
+
+TEST(WavReader_DataLength, IsProductOfChannels_BytesPerSample_and_Samples) {
+   uint32_t bytesPerSample{ 2 };
+   uint32_t samples { 5 };
+   uint32_t channels { 4 };
+
+   uint32_t length { reader.dataLength(bytesPerSample, samples, channels) };
+
+   CHECK_EQUAL(2 * 5 * 4, length);
+}
