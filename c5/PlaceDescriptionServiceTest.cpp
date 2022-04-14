6bf132f6250f7b7d2b5377b9614ee22355bcc4f6
Nihility <Nihility.t@gmail.com>
2022-04-15 00:34:47 +0800

    c5/19

--- c5/PlaceDescriptionServiceTest.cpp
+++ c5/PlaceDescriptionServiceTest.cpp
@@ -19,55 +19,36 @@ class APlaceDescriptionService: public Test {
 public:
    static const string ValidLatitude;
    static const string ValidLongitude;
-
-   shared_ptr<HttpStub> httpStub;
-   shared_ptr<HttpFactory> factory;
-   shared_ptr<PlaceDescriptionService> service;
-
-   virtual void SetUp() override {
-      factory = make_shared<HttpFactory>();
-      service = make_shared<PlaceDescriptionService>(factory);
-   }
-
-   void TearDown() override {
-      factory.reset();
-      httpStub.reset();
-   }
 };
 
 class APlaceDescriptionService_WithHttpMock: public APlaceDescriptionService {
 public:
-   void SetUp() override {
-      APlaceDescriptionService::SetUp();
-      httpStub = make_shared<HttpStub>();
-      factory->setInstance(httpStub);
-   }
+   PlaceDescriptionServiceTemplate<HttpStub> service;
 };
 
 TEST_F(APlaceDescriptionService_WithHttpMock, MakesHttpRequestToObtainAddress) {
+
    string urlStart{
       "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&"};
+	  
    auto expectedURL = urlStart + 
       "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
       "lon=" + APlaceDescriptionService::ValidLongitude;
-   EXPECT_CALL(*httpStub, initialize());
-   EXPECT_CALL(*httpStub, get(expectedURL));
-   service->summaryDescription(ValidLatitude, ValidLongitude);
+   EXPECT_CALL(service.http(), initialize());
+   EXPECT_CALL(service.http(), get(expectedURL));
+
+   service.summaryDescription(ValidLatitude, ValidLongitude);
 }
 
 class APlaceDescriptionService_WithNiceHttpMock: 
    public APlaceDescriptionService {
 public:
-   void SetUp() override {
-      APlaceDescriptionService::SetUp();
-      httpStub = make_shared<NiceMock<HttpStub>>();
-      factory->setInstance(httpStub);
-   }
+   PlaceDescriptionServiceTemplate<NiceMock<HttpStub>> service;
 };
 
 TEST_F(APlaceDescriptionService_WithNiceHttpMock, 
       FormatsRetrievedAddressIntoSummaryDescription) {
-   EXPECT_CALL(*httpStub, get(_))
+   EXPECT_CALL(service.http(), get(_))
       .WillOnce(Return(
          R"({ "address": {
               "road":"Drury Ln",
@@ -75,9 +56,8 @@ TEST_F(APlaceDescriptionService_WithNiceHttpMock,
               "state":"CO",
               "country":"US" }})"));
 
-   auto description = service->summaryDescription(ValidLatitude, ValidLongitude);
+   auto description = service.summaryDescription(ValidLatitude, ValidLongitude);
 
-   Mock::VerifyAndClearExpectations(httpStub.get());
    ASSERT_THAT(description, Eq("Drury Ln, Fountain, CO, US"));
 }
 
