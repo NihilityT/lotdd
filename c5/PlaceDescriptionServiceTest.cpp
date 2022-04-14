92cf27c679a67cba3de4e7f3e0af164bd7676377
Nihility <Nihility.t@gmail.com>
2022-04-15 00:34:45 +0800

    c5/14

--- c5/PlaceDescriptionServiceTest.cpp
+++ c5/PlaceDescriptionServiceTest.cpp
@@ -1,19 +1,12 @@
 #include "gmock/gmock.h"
 
+#include <memory>
+
 #include "PlaceDescriptionService.h"
 #include "Http.h"
 
 using namespace std;
 using namespace testing;
-
-class APlaceDescriptionService: public Test {
-public:
-   static const string ValidLatitude;
-   static const string ValidLongitude;
-};
-
-const string APlaceDescriptionService::ValidLatitude("38.005");
-const string APlaceDescriptionService::ValidLongitude("-104.44");
 
 class HttpStub: public Http {
 public:
@@ -21,13 +14,19 @@ public:
    MOCK_CONST_METHOD1(get, string(const string&));
 };
 
+class APlaceDescriptionService: public Test {
+public:
+   static const string ValidLatitude;
+   static const string ValidLongitude;
+};
+const string APlaceDescriptionService::ValidLatitude("38.005");
+const string APlaceDescriptionService::ValidLongitude("-104.44");
+
 TEST_F(APlaceDescriptionService, MakesHttpRequestToObtainAddress) {
    InSequence forceExpectationOrder;
    HttpStub httpStub;
-   
    string urlStart{
       "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&"};
-	  
    auto expectedURL = urlStart + 
       "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
       "lon=" + APlaceDescriptionService::ValidLongitude;
