8bd9c28d19bd0f41b2dfb6650e1889bb02fd2fe1
Nihility <Nihility.t@gmail.com>
2022-04-15 00:34:31 +0800

    c2/30

--- c2/Soundex.h
+++ c2/Soundex.h
@@ -43,7 +43,9 @@ private:
       return encoding.length() == MaxCodeLength - 1; 
    }
 
+public:
    std::string encodedDigit(char letter) const {
+      // ...
       const std::unordered_map<char, std::string> encodings {
          {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
          {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"},
@@ -57,6 +59,8 @@ private:
       return it == encodings.end() ? "" : it->second;
    }
 
+private:
+   // ...
    std::string zeroPad(const std::string& word) const {
       auto zerosNeeded = MaxCodeLength - word.length();
       return word + std::string(zerosNeeded, '0');
